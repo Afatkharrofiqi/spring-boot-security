9c25d422d7eec630361e12ca220c03d391d7a24b
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2023-04-20 05:18:41 +0700

    feat: jwt authentication

---
+++ src/test/java/com/fatkharrofiqi/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatkharrofiqi.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
